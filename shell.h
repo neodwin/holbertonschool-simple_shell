003ff9e0cea474d6ac0af29b13901e77d80cb853
Frederic Bourouliou <bourouliou@gmail.com>
2024-12-19 20:42:35 +0100

    update path et shell.h

--- shell.h
+++ shell.h
@@ -9,13 +9,20 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 /* Prototypes */
+extern char **environ;
 char *_getenv(const char *value);
 void display_prompt(void);
 char *get_input(void);
 void execute_command(char *input);
 void handle_env(void);
+char *find_path_in_environ(void);
+char *check_current_path(char *command);
+char *try_path(char *dir, char *command);
+char *get_command_path(char *command);
 
 /* Global variable */
 extern char **environ;
