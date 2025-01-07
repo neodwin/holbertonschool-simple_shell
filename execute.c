c61df8bd8b64f47aa065d0c2edddb60653dd7e1b
Frederic Bourouliou <bourouliou@gmail.com>
2025-01-07 19:22:40 +0100

    fix: update error message format to match sh output

--- execute.c
+++ execute.c
@@ -7,7 +7,7 @@
  */
 void handle_command_error(const char *program_name, const char *command)
 {
-	fprintf(stderr, "%s: 1: %s: not found\n", program_name, command);
+	fprintf(stderr, "%s: line 1: %s: No such file or directory\n", program_name, command);
 }
 
 /**
